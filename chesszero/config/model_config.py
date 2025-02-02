cbd360dbe4edbe62809020659129710b36df75ec
Jakob Frick <jakob.maria.frick@gmail.com>
2025-02-01 19:05:34 -0800

    update

--- chesszero/config/model_config.py
+++ chesszero/config/model_config.py
@@ -18,7 +18,7 @@ class ModelConfig:
     policy_output_dim: int = 4672  # All possible moves: 8x8x73
 
     # MCTS
-    n_simulations: int = 1600  # Reduced number of simulations for faster training
+    n_simulations: int = 800  # Reduced number of simulations for faster training
     c_puct: float = 1.0  # Exploration constant
     dirichlet_alpha: float = 0.3  # Dirichlet noise parameter
     dirichlet_epsilon: float = 0.25  # Weight of Dirichlet noise

67e27ee7b6492d85668fc0171ebb53e822d8ab02
Jakob Frick <jakob.maria.frick@gmail.com>
2025-02-04 11:47:28 -0800

    no negative 0

--- chesszero/model/self_play.py
+++ chesszero/model/self_play.py
@@ -49,7 +49,10 @@ def play_single_game(
 
         # Process game results
         white_result = game.board.get_game_result(perspective_color=0)
-        black_result = -1 * white_result
+        if white_result == 0:
+            black_result = 0
+        else:
+            black_result = -1 * white_result
 
         # Send results back through queue
         result_queue.put(
